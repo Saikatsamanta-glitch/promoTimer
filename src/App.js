638749d5a1fdd667c0742599ff3214bfaf8e3e94
saikat samanta <saikat.samanta@yapsody.com>
2024-01-24 11:35:12 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [barstyle, setBarStyle] = useState("bees");
+  const styles = ["crosses","jupiter","piano","dominos","pie","bees","food","floor","wiggle","bars","bubbles","ticTac","zigZag","stripes","clouds","aztec","circuit"];
+  const [prog,setProg] = useState(0);
+  const changestyle =(e)=>{
+    setBarStyle(e.target.value)
+  }
+
+  const ondrag = (e)=>{
+    console.log(e.clientX);
+    setProg(e.clientX)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="progress">
+        <div style={{width:`${prog}px`}} className={`bar shadow ${barstyle}`}> <div className='selectable' onDrag={ondrag} draggable></div> </div>
+      </div>
+      <select className='selection' onChange={changestyle}>
+        {
+          styles.map((v,i)=>{
+            return <option key={i} value={v}> {v} </option>
+          })
+        }
+      </select>
+    </>
   );
 }
 
