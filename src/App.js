2c4f91fcbf84b1dd392e339c756157d671d9042a
saikat samanta <saikat.samanta@yapsody.com>
2024-01-29 10:14:54 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import { useState } from 'react';
 function App() {
   const [barstyle, setBarStyle] = useState("bees");
   const styles = ["crosses", "jupiter", "piano", "dominos", "pie", "bees", "food", "floor", "wiggle", "bars", "bubbles", "ticTac", "zigZag", "stripes", "clouds", "aztec", "circuit"];
-  const [prog, setProg] = useState(60);
-  const changestyle=(e)=>{setBarStyle(e.target.value)};
-  const handler=(e)=>{
+  const [prog, setProg] = useState(7.5*30);
+  const changestyle = (e) => { setBarStyle(e.target.value) };
+  const handler = (e) => {
     const startSize = prog;
-    const startPosition = e.pageX ;
-    console.log("startSize",startSize,"postion",startPosition)
+    const startPosition = e.pageX;
+    console.log("startSize", startSize, "postion", startPosition)
     function onMouseMove(mouseMoveEvent) {
-      if(startSize-startPosition+mouseMoveEvent.pageX<=900 && startSize-startPosition+mouseMoveEvent.pageX>=0){
-        setProg(startSize-startPosition+mouseMoveEvent.pageX)
+      if (startSize - startPosition + mouseMoveEvent.pageX <= 900 && startSize - startPosition + mouseMoveEvent.pageX >= 0) {
+        setProg(startSize - startPosition + mouseMoveEvent.pageX)
       }
-      
+
     }
     function onMouseUp() {
       document.body.removeEventListener("mousemove", onMouseMove);
     }
-    document.body.addEventListener('mousemove',onMouseMove )
-    document.body.addEventListener('mouseup', onMouseUp,{once:true})
+    document.body.addEventListener('mousemove', onMouseMove)
+    document.body.addEventListener('mouseup', onMouseUp, { once: true })
   }
 
-console.log(prog)
+  console.log(prog)
 
   return (
     <>
       <div className="progress">
         <div style={{ width: `${prog}px` }} className={`bar shadow ${barstyle}`}>
-            <button id='dragslide' onMouseDown={handler}> {(prog/7.5).toFixed(1)} </button>
+          <button id='dragslide' onMouseDown={handler}> {(prog / 7.5).toFixed(1)} </button>
         </div>
       </div>
 
@@ -39,11 +39,15 @@ console.log(prog)
           })
         }
       </select>
-<div className='customtimers '>
-        <button className='selection' onClick={()=>setProg(225)}>30min</button>
-        <button className='selection' onClick={()=>{setProg(450)}}>1hour</button>
-        <button className='selection' onClick={()=>{setProg(900)}}>2hour</button>
-</div>
+      <div className='customtimers '>
+        <button className='selection' onClick={() => setProg(225)}>30min</button>
+        <button className='selection' onClick={() => { setProg(450) }}>1hour</button>
+        <button className='selection' onClick={() => { setProg(900) }}>2hour</button>
+      </div>
+      <div className='btncustom'>
+        <button className='selections' onClick={() => setProg(prog-15*7.5)}>-15min</button>
+        <button className='selections' onClick={() => { setProg(prog+15*7.5) }}>+15min</button>
+      </div>
     </>
   );
 }
